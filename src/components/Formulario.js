bf3f3a266d133fbb4c67e6b0267e06996987ff29
Marcos Ahumada <marcos.ahumada@example.com>
2019-09-04 15:27:36 -0400

    input select button

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -13,25 +13,25 @@ export default class Formulario extends Component {
                         Busca tu evento por Nombre o Categoría
                     </legend>
                 </fieldset>
-                <div className="uk-column-1-3@m " uk-margin="true">
-                    <input 
-                        type="text" name="nombre" 
-                        className="uk-input" placeholder="Nombre de Evento o Ciudad" 
-                    />
-                    
-                </div>
-                <div className="uk-column-1-3@m " uk-margin="true">
-                    <select 
-                        type="text" name="categoria" 
-                        className="uk-select" placeholder="" 
-                    >
+                <div className="uk-column-1-3@m" >
+                    <div className="uk-margin" uk-margin="true">
+                        <input 
+                            type="text" name="nombre" 
+                            className="uk-input" placeholder="Nombre de Evento o Ciudad" 
+                        />
+                    </div>
+                    <div className="uk-margin" uk-margin="true">
+                        <select 
+                            type="text" name="categoria" 
+                            className="uk-select" placeholder="" 
+                        >
 
-                    </select>
-                    
-                </div>
-                <div>
-                    <button type="button" className="uk-button uk-button-danger"></button>
-                </div>
+                        </select>
+                    </div>
+                    <div>
+                        <input type="submit" className="uk-button uk-button-danger" value="Buscar Eventos"/>
+                    </div>                                         
+                </div>            
             </form>
         )
     }
